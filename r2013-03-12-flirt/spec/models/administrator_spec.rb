13405644455361d7d34d0ce3d6efaffec2100ca6
Alex Fogg <me@alexfogg.com>
2013-03-14 12:01:03 -0400

    deploy 2

--- r2013-03-12-flirt/spec/models/administrator_spec.rb
+++ r2013-03-12-flirt/spec/models/administrator_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: administrators
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  role       :string(255)
+#  ssn        :string(255)
+#  tel        :string(255)
+#
+
 require 'spec_helper'
 
 describe Administrator do
@@ -19,8 +31,23 @@ describe Administrator do
 
   describe '.create' do
     it 'has an id' do
-      administrator = Administrator.create
+      administrator = Administrator.create(role: 'db')
       expect(administrator.id).to_not be nil
+    end
+
+    it 'role fails validation when blank' do
+      administrator = Administrator.create
+      expect(administrator.id).to be nil
+    end
+  end
+
+  describe '#metadata' do
+    it 'has administrator properties' do
+      administrator = Administrator.create(role: 'db', ssn: '111-22-3333', tel: '1-111-222-3333')
+      expect(administrator.id).to_not be nil
+      expect(administrator.role).to eq 'db'
+      expect(administrator.ssn).to eq '111-22-3333'
+      expect(administrator.tel).to eq '1-111-222-3333'
     end
   end
 end
